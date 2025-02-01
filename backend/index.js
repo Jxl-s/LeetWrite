18a5c0ed6adb5f5f7cc728f60820056959b8fcaa
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-02-01 12:09:00 -0500

    init backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+	res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", socket => {
+	console.log("a user connected");
+});
+
+const PORT = process.env.PORT ?? 5050;
+server.listen(PORT, () => {
+	console.log(`server running at http://localhost:${PORT}`);
+});
